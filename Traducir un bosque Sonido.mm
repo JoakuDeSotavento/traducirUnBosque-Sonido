d5aac27b4b0b7ac3d5c97ad1a18214a9f402dcf8
Joaku De Sotavento <joaquindiazescultor@gmail.com>
2021-02-18 14:11:21 +0100

    Osc

---
+++ Traducir un bosque Sonido.mm
@@ -0,0 +1,151 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1613500946343" ID="ID_34798651" MODIFIED="1613502950979" TEXT="Traducir un bosque Sonido">
+<edge COLOR="#000000" WIDTH="8"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#0033ff" CREATED="1613500975974" ID="ID_75688541" MODIFIED="1613502950979" POSITION="right" TEXT="Guitarras del 1 al 5">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#00b439" CREATED="1613501201411" ID="ID_261992064" MODIFIED="1613502950979" TEXT="Volumen">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#00b439" CREATED="1613501537261" ID="ID_1278347006" MODIFIED="1613502950979" TEXT="Distorsi&#xf3;n (control de voltaje)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#990000" CREATED="1613501552436" ID="ID_697795187" MODIFIED="1613502950980" TEXT="frecuencia a la que se aplica la distorsi&#xf3;n">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1613501545751" ID="ID_1338835739" MODIFIED="1613502950980" TEXT="grado de distorsi&#xf3;n">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1613501608066" ID="ID_991863246" MODIFIED="1613502950980" TEXT="Reverb">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1613501081530" ID="ID_1882856083" MODIFIED="1613502950981" POSITION="right" TEXT="Anti Guitarras 1 y 2">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#00b439" CREATED="1613501638581" ID="ID_1535831923" MODIFIED="1613502950981" TEXT="Volumen">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#00b439" CREATED="1613501537261" ID="ID_465468053" MODIFIED="1613502950981" TEXT="Distorsi&#xf3;n (control de voltaje)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#990000" CREATED="1613501552436" ID="ID_472922562" MODIFIED="1613502950981" TEXT="frecuencia a la que se aplica la distorsi&#xf3;n">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1613501545751" ID="ID_1833578817" MODIFIED="1613502950982" TEXT="grado de distorsi&#xf3;n">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1613501000781" ID="ID_1994969251" MODIFIED="1613502950982" POSITION="right" TEXT="Mar Malaga Low FQ">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#00b439" CREATED="1613501638581" ID="ID_206328926" MODIFIED="1613502950982" TEXT="Volumen">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#00b439" CREATED="1613501537261" ID="ID_1003010943" MODIFIED="1613502950983" TEXT="Distorsi&#xf3;n (control de voltaje)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#990000" CREATED="1613501552436" ID="ID_1109293496" MODIFIED="1613502950983" TEXT="frecuencia a la que se aplica la distorsi&#xf3;n">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1613501545751" ID="ID_1928081452" MODIFIED="1613502950984" TEXT="grado de distorsi&#xf3;n">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1613501090701" ID="ID_1965005011" MODIFIED="1613502950984" POSITION="right" TEXT="Lluvia pl&#xe1;sticos 1, 2 y 3">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#00b439" CREATED="1613501638581" ID="ID_1792319839" MODIFIED="1613502950984" TEXT="Volumen">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#00b439" CREATED="1613501537261" ID="ID_1634236566" MODIFIED="1613502950984" TEXT="Distorsi&#xf3;n (paso bandas)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#990000" CREATED="1613501552436" ID="ID_698493913" MODIFIED="1613502950985" TEXT="frecuencia a la que se aplica la distorsi&#xf3;n">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1613501545751" ID="ID_924343901" MODIFIED="1613502950985" TEXT="grado de distorsi&#xf3;n">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1613502154085" ID="ID_191581644" MODIFIED="1613502950985" POSITION="right" TEXT="Free Arpeggiator (+/- 10 samples)">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#00b439" CREATED="1613501638581" ID="ID_1821225898" MODIFIED="1613502950986" TEXT="Volumen">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#00b439" CREATED="1613501537261" ID="ID_140545507" MODIFIED="1613502950986" TEXT="Distorsi&#xf3;n (paso altas)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#990000" CREATED="1613501552436" ID="ID_1973226416" MODIFIED="1613502950986" TEXT="frecuencia a la que se aplica la distorsi&#xf3;n (aleatorio)">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1613501545751" ID="ID_1661319902" MODIFIED="1613502950987" TEXT="Frecuencia de disparo entre samples">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1613502241040" ID="ID_1117933036" MODIFIED="1613502950987" POSITION="right" TEXT="Free percusions">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#00b439" CREATED="1613501638581" ID="ID_294031748" MODIFIED="1613502950987" TEXT="Volumen">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#00b439" CREATED="1613501537261" ID="ID_1914575552" MODIFIED="1613502950987" TEXT="Distorsi&#xf3;n (paso altas)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#990000" CREATED="1613501552436" ID="ID_73987886" MODIFIED="1613502950988" TEXT="frecuencia a la que se aplica la distorsi&#xf3;n (aleatorio)">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1613501545751" ID="ID_687526332" MODIFIED="1613502950988" TEXT="Frecuencia de disparo entre samples">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1613502257122" ID="ID_1468287512" MODIFIED="1613502950988" POSITION="right" TEXT="Hoja de papel">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#00b439" CREATED="1613501638581" ID="ID_416812641" MODIFIED="1613502950989" TEXT="Volumen">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#00b439" CREATED="1613501537261" ID="ID_696616936" MODIFIED="1613502950989" TEXT="Distorsi&#xf3;n (paso altas)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+<node COLOR="#990000" CREATED="1613501552436" ID="ID_1323595770" MODIFIED="1613502950990" TEXT="frecuencia a la que se aplica la distorsi&#xf3;n (aleatorio)">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1613501545751" ID="ID_970586707" MODIFIED="1613502950990" TEXT="Frecuencia de disparo entre samples">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+</node>
+</map>
